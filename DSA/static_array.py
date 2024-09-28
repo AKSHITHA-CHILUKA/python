1d7376636ba2e2c1282c3fe0a4c5236ae29e4ce6
~Chiluka Akshitha <22wh1a12b5@bvrithyderabad.edu.in>
2024-09-28 20:01:01 +0530

    Create static_array.py

---
+++ DSA/static_array.py
@@ -0,0 +1,6 @@
+arr = [22,346,1,-4]
+print(arr[0]) #printing arr 0
+arr[2] = 12 #assigning 12 to 2nd index
+print(arr) #printing the new array with changed value
+print(arr[2]) #pring the newly assigned value
+#print(arr[9]) array index out of range
