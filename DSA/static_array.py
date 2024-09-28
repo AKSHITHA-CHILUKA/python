3ba6f048aa128cc23ce24dfead6d92f465f340b3
~Chiluka Akshitha <22wh1a12b5@bvrithyderabad.edu.in>
2024-09-28 20:05:11 +0530

    Update static_array.py

--- DSA/static_array.py
+++ DSA/static_array.py
@@ -4,3 +4,7 @@ arr[2] = 12 #assigning 12 to 2nd index
 print(arr) #printing the new array with changed value
 print(arr[2]) #pring the newly assigned value
 #print(arr[9]) array index out of range
+arr.append(19) #adds 19
+print(arr)
+arr.remove(22) #removes 22
+print(arr)
